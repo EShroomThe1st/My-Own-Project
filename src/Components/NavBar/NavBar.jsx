87f42b1b47ce9bec862cd85e18142760a15692d8
EShroomThe1st <xxxthedestroyer058xxx@gmail.com>
2023-06-21 18:44:11 +0700

    NO

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,32 @@
 import { Stack } from '@mui/material';
 import {NavOptions} from '../../Share/NavOptions';
-import './NavBar.css'
+import styles from './NavBar.module.css'
 import { Link } from 'react-router-dom';
+import { Brightness7, BusinessCenter, DeviceHub, Home } from '@mui/icons-material';
 
 const NavBar = () => (
-    <Stack direction='row' className='Nav'>
+    <Stack direction='row' className={styles.Nav}>
         <Stack direction='row' >
             <ul>
-            {NavOptions.map((option)=>(
-                <li key={option.name}><Link className={option.name} 
-                to={option.link}>{option.icon}{option.name}</Link></li>
-            ))}
+                <li><Link className={styles.Home}
+                to="/home"><Home/>Home</Link></li>
+                
+                <li><Link className={styles.Red} 
+                to="/red"><Brightness7/>Red</Link></li>
+                
+                <li><Link className={styles.Yellow} 
+                to="/yellow"><DeviceHub/>Yellow</Link></li>
+                
+                <li><Link className={styles.Blue}
+                to="/blue"><BusinessCenter/>Blue</Link></li>
             </ul>
         </Stack>
         <Stack>
-        <a href='home'><img className='Icon' src='/asset/img/Page-Logo.png' alt=''/></a>
+        <a href='home'><img className={styles.Icon} src='/asset/img/Page-Logo.png' alt=''/></a>
         </Stack>
-        <Stack className='Search' direction='row'>
+        <Stack className={styles.Search} direction='row'>
             <input type='text' id='SearchBox' name='SearchBox' placeholder='Search Box'></input>
-            <img className='SearchIcon' src='/asset/img/1200px-VisualEditor_-_Icon_-_Search-big_-_white.svg.png' alt=''/>
+            <img className={styles.SearchIcon} src='/asset/img/1200px-VisualEditor_-_Icon_-_Search-big_-_white.svg.png' alt=''/>
         </Stack>
     </Stack>
 )
